45c4e40909ef43b62f4e89cd7923a1ae6653587c
EnchantedNight <neffex.god9@gmail.com>
2026-04-10 18:21:10 +0000

    add fallback database url

--- lib/utils/prisma.ts
+++ lib/utils/prisma.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from "@lib/prisma/client";
 
 export const client = new PrismaClient({
   adapter: new PrismaNeon({
-    connectionString: process.env.DATABASE_URL,
+    connectionString:
+      process.env.DATABASE_URL || "postgresql://user:password@host:5432/next",
   }),
 });
